b4bb2d5cfafe0c3af4a0ad64143da2f0dc166ddd
jayme-github <jayme-github@users.noreply.github.com>
2020-10-17 17:20:50 +0200

    Fix RemovedInSphinx40Warning
    The app.add_stylesheet() is deprecated. Please use app.add_css_file() instead.

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@ exec(open("../src/paperless/version.py").read())
 
 # Believe it or not, this is the officially sanctioned way to add custom CSS.
 def setup(app):
-    app.add_stylesheet("custom.css")
+    app.add_css_file("custom.css")
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
